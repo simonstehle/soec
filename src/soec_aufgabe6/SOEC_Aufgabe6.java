9206a3d30261bc55d348018230cbf3817e89dcd3
ss-162166 <ss-162166@AP086173.hrw.hs-weingarten.de>
2017-05-12 14:49:25 +0200

    added more guis

---
+++ src/soec_aufgabe6/SOEC_Aufgabe6.java
@@ -0,0 +1,18 @@
+/*
+ */
+package soec_aufgabe6;
+
+/**
+ *
+ * @author ss-162166
+ */
+public class SOEC_Aufgabe6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
