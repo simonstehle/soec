5379de1cfddb6cc2a11c552fa4d1db0fa0facae3
simon stehle <ss-162166@hs-weingarten.de>
2017-05-13 08:18:53 +0200

    tested with closing panels

--- soec/src/soec/Soec.java
+++ soec/src/soec/Soec.java
@@ -5,17 +5,24 @@
  */
 package soec;
 
+import gui.gui_search_booking;
 /**
  *
  * @author simon
  */
 public class Soec {
+     
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new gui_search_booking().setVisible(true);
+            }
+        });
     }
     
 }
